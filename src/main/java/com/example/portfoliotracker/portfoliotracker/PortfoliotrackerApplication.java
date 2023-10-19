8ddbeca00fc0bfded335f3ad2b7e9f9c23eed80e
divyansh <mah.divyansh@gmail.com>
2023-10-19 14:50:11 +0530

    fix deployement issues

--- src/main/java/com/example/portfoliotracker/portfoliotracker/PortfoliotrackerApplication.java
+++ src/main/java/com/example/portfoliotracker/portfoliotracker/PortfoliotrackerApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 public class PortfoliotrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfoliotrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PortfoliotrackerApplication.class, args);
+    }
 
 }
